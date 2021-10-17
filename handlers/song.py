4c3cb353536b29fc6e61775ba6342619519e6000
Abhimnayu Singh Ranawat <92500846+AbhimanyuSinghRana@users.noreply.github.com>
2021-10-17 17:13:12 +0530

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -61,7 +61,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = "**🎧 ᴛʜɪs sᴏɴɢ ɪs ᴜᴘʟᴏᴀᴅᴇᴅ ᴠɪᴀ\n💥 [ᴀᴅɪᴛʏᴀ sᴇʀᴠᴇʀ](t.me/adityaserver) ...**"
+        rep = "**🎧 ᴛʜɪs sᴏɴɢ ɪs ᴜᴘʟᴏᴀᴅᴇᴅ ᴠɪᴀ\n💥 [ᴀʟɪꜱʜᴀ sᴇʀᴠᴇʀ](t.me/Venom_Hai_Hum) ...**"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
@@ -76,7 +76,7 @@ def song(client, message):
         )
         m.delete()
     except Exception as e:
-        m.edit("**📶 ᴀᴅɪᴛʏᴀ sᴇʀᴠᴇʀ ɪs ʙᴜsʏ, sᴏ\n🔀 ᴛʀʏ ᴀғᴛᴇʀ sᴏᴍᴇ ᴛɪᴍᴇ ...**")
+        m.edit("**📶 ᴀʟɪꜱʜᴀ sᴇʀᴠᴇʀ ɪs ʙᴜsʏ, sᴏ\n🔀 ᴛʀʏ ᴀғᴛᴇʀ sᴏᴍᴇ ᴛɪᴍᴇ ...**")
         print(e)
 
     try:
